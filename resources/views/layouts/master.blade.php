bf749656278e6313b55fd7750036807689ccf5cf
zulfarobbani <robbanizulfa86@gmail.com>
2022-01-10 17:36:18 +0700

    tambah database

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Web Tracer Alumni</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<style>
+		svg {
+			margin: 0 10px;
+		}
+		.fg a[href] {
+			text-decoration: none;
+			color: black;
+		}
+		.fg a[href]:hover {
+			color: lightseagreen;
+			transition: 0.5s;
+		}
+		td {
+			position: relative;
+			bottom: 10px;
+		}
+		p {
+			text-indent: 2rem;
+		}
+	</style>
+</head>
+<body>
+	<main>
+		<div class="flex">
+			<div class="fg">
+				<img src="OIP.jpg" style="border-radius: 100%;" width="150px">
+			</div>
+
+			<div class="fg" style="flex-grow: 7;">
+				<h1>Web Tracer Alumni SMKN 4 Bandung</h1>
+				<p style="position: relative;right: 30px;bottom: 10px;">Lihat Alumni Secara Online</p>
+			</div>
+		</div>
+		<br>
+		<hr>
+
+		<center>
+			<div class="flex" style="padding: 15px;">
+				<div class="fg">
+					<b><a href="index.html" style="color: lightseagreen;">Dashboard</a></b>
+				</div>
+				<div class="fg">
+					<b><a href="login.html">Login/Register</a></b>
+				</div>
+				<div class="fg">
+					<b><a href="profile.html">Profile</a></b>
+				</div>
+				<div class="fg">
+					<b><a href="alumni.html">Data Alumni</a></b>
+				</div>
+				<div class="fg">
+					<b><a href="post.html">Postingan</a></b>
+				</div>
+			</div>
+		</center>
+		<hr>
+		<br>
+
+		<article class="card">
+			<center>
+			<h3>Selamat Datang di Web Tracer Alumni</h3>
+			</center>
+		</article>
+
+		<article>
+			<p style="display: flex;">
+				<span style="flex-grow: 1;" class="card">
+					Alumni Tahun 2017 : <b>215 Orang</b>
+				</span>
+				<span style="flex-grow: 1;" class="card">
+					Alumni Tahun 2018 : <b>329 Orang</b>
+				</span>
+				<span style="flex-grow: 1;" class="card">
+					Alumni Tahun 2019 : <b>441 Orang</b>
+				</span>
+			</p>
+		</article>
+	</main>
+
+	<footer>
+		<p style="color: #434343;">
+			<a>About</a> &nbsp; <a>Contact Us</a> &nbsp;<a>Help</a> &nbsp; <a>Privacy</a> &nbsp; <a>Terms</a> &nbsp;
+		</p>
+		<p>copyright 2022 | <span style="color: lightseagreen;">SMK NEGERI 4 BANDUNG</span></p>
+	</footer>
+</body>
+</html>
